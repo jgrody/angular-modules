b70cc582114e3e8aa0eab0af4953642fbe2b8645
Jack Grossmann <jtg028@gmail.com>
2016-03-15 21:14:10 -0400

    Update path/name

--- sanitized/directive.js
+++ sanitized/directive.js
@@ -2,7 +2,7 @@ module.exports = function() {
   "ngInject";
 
   return {
-    templateUrl: '/angular-modules/sanitized/template.html',
+    templateUrl: '/modules/sanitized/template.html',
     scope: { sanitized: '=' },
     controller: 'SanitizedController'
   }
